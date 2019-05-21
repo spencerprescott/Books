9fc88ecfc710b62e4f2c672499f17c59f1b4a9b8
Spencer Prescott <stp@umail.ucsb.edu>
2019-05-20 23:36:57 -0700

    Initial Commit

---
+++ Books/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Books
+//
+//  Created by Spencer Prescott on 5/20/19.
+//  Copyright © 2019 Spencer Prescott. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
